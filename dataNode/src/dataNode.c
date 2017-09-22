9589c60cfab1f9ff83c3103ab8ce770e6a821102
Lucas <l.a.galeano2@gmail.com>
2017-09-22 14:56:53 -0300

    ordeno un poco el codigo

--- dataNode/src/dataNode.c
+++ dataNode/src/dataNode.c
@@ -4,7 +4,7 @@
  Author      : 
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : Proceso dataNode
  ============================================================================
  */
 
@@ -27,8 +27,14 @@ int main(void) {
 
 
 	t_config* conf;
-    conf = config_create("../configuraciones/nodo.cfg");                 // path relativo al archivo nodo.cfg
+	char* bloque;
+	t_log_level logLevel = LOG_LEVEL_INFO;               //elijo enum de log
+    t_log* logger = log_create("dataNode_log", "dataNode", true, logLevel ); //creo archivo log
 
+
+	//LEER ARCHIVO DE CONFIGURACION ---------------------------------------------------------
+
+    conf = config_create("../configuraciones/nodo.cfg");                 // path relativo al archivo nodo.cfg
     char* IP_FILESYSTEM = config_get_string_value(conf,"IP_FILESYSTEM");        // traigo los datos del archivo nodo.cfg
     char* PUERTO_FILESYSTEM = config_get_string_value(conf,"PUERTO_FILESYSTEM");
     int NOMBRE_NODO = config_get_string_value(conf,"NOMBRE_NODO");
@@ -36,14 +42,16 @@ int main(void) {
     int PUERTO_DATANODE = config_get_int_value(conf,"PUERTO_DATANODE");
     char* RUTA_DATABIN = config_get_string_value(conf,"RUTA_DATABIN");
 
-    t_log_level logLevel = LOG_LEVEL_INFO;               //elijo enum
+
     t_log* logger = log_create("dataNode_log", "dataNode", true, logLevel ); //creo archivo log
 
     //log_warning(logger, "algo paso aca!!!!!");
 
-    char* bloque = getBloque(2);
 
-    puts(bloque);
+    bloque = getBloque(2);
+
+    //CONECTARSE A FILESYSTEM, QUEDAR A LA ESPERA DE SOLICITUDES --------------------------------
+
 	return EXIT_SUCCESS;
 
 }
@@ -52,33 +60,33 @@ int main(void) {
 char* getBloque(int numBloque)
 {
 	struct stat sb;
-	int *map;
-    char *bloque  = calloc(9,1);
+	char *map;
+    char *bloque  = calloc(TAMBLOQUE,1);
 
 	int fd = open(PATHDATA,	O_RDONLY); //abrir archivo data.bin
 
 	fstat(fd, &sb);
 
-    map = mmap(NULL,        //donde comienza a guardar el mapeo, NULL significa "donde quiera el S.O"
-    		sb.st_size,		//el tamaño del file
-			PROT_READ,		//proteccion del file (PROT_READ = solo lectura)
-			MAP_SHARED,		//que comparta el mapeo con otros procesos creo, no se bien que hace
-			fd,				//el file descriptor
-			0);				//desde donde leer
+    map = (char*) mmap(NULL,        //donde comienza a guardar el mapeo, NULL significa "donde quiera el S.O"
+    			sb.st_size,		//el tamaño del file
+				PROT_READ,		//proteccion del file (PROT_READ = solo lectura)
+				MAP_SHARED,		//que comparta el mapeo con otros procesos creo, no se bien que hace
+				fd,				//el file descriptor
+				0);				//desde donde leer
 
-	char* a = (char*)map;
+
 	int i = numBloque*TAMBLOQUE - TAMBLOQUE;
 	int j = 0;
 	for(i; i<(numBloque*TAMBLOQUE); i++ )
 	{
-		bloque[j] = a[i];
+		bloque[j] = map[i];
 		j++;
 	}
 
 
-    munmap(map,sb.st_size);
-    close(fd);
-
+    munmap(map,sb.st_size);  //cierro mmap()
+    close(fd);				//cierro archivo
+    free(bloque);		//libero bloque
     return bloque;
 
 }
