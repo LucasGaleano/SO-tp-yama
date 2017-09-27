ef7aacee359c2a19e258e2358dd0c624a346c78b
ahufa <facundoahumada93@hotmail.com>
2017-09-26 21:59:25 -0300

    Se crea un hilo que atiende conexiones y en el main queda ejecutando la consola

--- fileSystem/src/fileSystem.c
+++ fileSystem/src/fileSystem.c
@@ -1,43 +1,70 @@
 #include "fileSystem.h"
 
-int main() {
+int main(int argc, char **argv) {
+	//Creo archivo de log
+	logFileSystem = log_create("log_FileSystem.log", "fileSystem", false,
+			LOG_LEVEL_TRACE);
+	log_info(logFileSystem, "Inicio el proceso fileSystem \n");
 
-	//iniciarServer("3200", (void *) procesarPaquete);
+	//Creo el thread para escuchar conexiones
+	pthread_t threadServer;
 
+	if (pthread_create(&threadServer, NULL, (void*) iniciarServidor,
+			PUERTO_FILESYSTEM)) {
+		perror("Error el crear el thread servidor.");
+		exit(EXIT_FAILURE);
+	}
+
+	//Levanto consola
 	iniciarConsola();
+
+	pthread_join(threadServer, NULL);
+
+	//Termina fileSystem
+	log_info(logFileSystem, "Termino el proceso fileSystem");
+
+	//Destruo archivo de log
+	log_destroy(logFileSystem);
 
 	return EXIT_SUCCESS;
 }
 
 /*-------------------------Procesamiento paquetes-------------------------*/
-void recibirHandshake(t_paquete * unPaquete, int * client_socket){
+void recibirHandshake(t_paquete * unPaquete, int * client_socket) {
 	int tipoCliente;
 	memcpy(&tipoCliente, unPaquete->buffer->data, sizeof(int));
 
 	switch (tipoCliente) {
-		case DATANODE:
-			break;
-		case YAMA:
-			break;
-		default:
-			*client_socket = -1;
-			break;
+	case DATANODE:
+		break;
+	case YAMA:
+		break;
+	case WORKER:
+		break;
+	default:
+		*client_socket = -1;
+		break;
 	}
 }
 
-void procesarPaquete(t_paquete * unPaquete, int * client_socket){
+void procesarPaquete(t_paquete * unPaquete, int * client_socket) {
 	switch (unPaquete->codigoOperacion) {
-		case HANDSHAKE:
-			recibirHandshake(unPaquete, client_socket);
-			break;
-		case ENVIAR_MENSAJE:
-			recibirMensaje(unPaquete);
-			break;
-		case ENVIAR_ARCHIVO:
-			recibirArchivo(unPaquete);
-			break;
-		default:
-			break;
+	case HANDSHAKE:
+		recibirHandshake(unPaquete, client_socket);
+		break;
+	case ENVIAR_MENSAJE:
+		recibirMensaje(unPaquete);
+		break;
+	case ENVIAR_ARCHIVO:
+		recibirArchivo(unPaquete);
+		break;
+	default:
+		break;
 	}
 	destruirPaquete(unPaquete);
+}
+
+/*-------------------------Funciones auxiliares-------------------------*/
+void iniciarServidor(char* unPuerto) {
+	iniciarServer(unPuerto, (void *) procesarPaquete);
 }
--- fileSystem/src/fileSystem.h
+++ fileSystem/src/fileSystem.h
@@ -1,13 +1,25 @@
 #ifndef FILESYSTEM_H_
 #define FILESYSTEM_H_
 
+#include <biblioteca/sockets.h>
+#include <commons/log.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 #include "consola.h"
-#include <biblioteca/sockets.h>
+
+/*---------	---------------Variables globales-------------------------*/
+t_log* logFileSystem;
+
+/*---------	---------------Estructuras-------------------------*/
+#define PUERTO_FILESYSTEM "3200"
 
 /*---------	---------------Procesamiento paquetes-------------------------*/
 void 				procesarPaquete					(t_paquete *, int *);
 void 				recibirHandshake				(t_paquete *, int *);
+
+/*-------------------------Funciones auxiliares-------------------------*/
+void 				iniciarServidor					(char*);
 
 #endif /* FILESYSTEM_H_ */
