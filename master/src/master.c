26dba2c3e501488cbb158f727bef5e23ccbfb9fa
MatiasGasparini <matias_gasparini@hotmail.com>
2017-10-21 01:33:13 -0300

    meto comienzo para manejar señales para los errores de los worker

--- master/src/master.c
+++ master/src/master.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- /*
+
  ============================================================================
  Name        : master.c
  Author      : 
@@ -14,17 +14,69 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <commons/config.h>
-#include "estructurasWorkerMaster.h"
-#include "estructurasMasterYama.h"
+#include <biblioteca/estructurasMasterYama.h>
+#include <biblioteca/estructurasWorkerMaster.h>
 
+/*
+
+Señales extraidas de /usr/bin/include/bits/signum.h
+
+
+
+#define SIG_ERR	((__sighandler_t) -1)		/* Error return.
+#define SIG_DFL	((__sighandler_t) 0)		/* Default action.
+#define SIG_IGN	((__sighandler_t) 1)		/* Ignore signal.
+
+/* Signals.
+#define	SIGHUP		1	/* Hangup (POSIX).
+#define	SIGINT		2	/* Interrupt (ANSI).
+#define	SIGQUIT		3	/* Quit (POSIX).
+#define	SIGILL		4	/* Illegal instruction (ANSI).
+#define	SIGTRAP		5	/* Trace trap (POSIX).
+#define	SIGABRT		6	/* Abort (ANSI).
+#define	SIGIOT		6	/* IOT trap (4.2 BSD).
+#define	SIGBUS		7	/* BUS error (4.2 BSD).
+#define	SIGFPE		8	/* Floating-point exception (ANSI).
+#define	SIGKILL		9	/* Kill, unblockable (POSIX).
+#define	SIGUSR1		10	/* User-defined signal 1 (POSIX).
+#define	SIGSEGV		11	/* Segmentation violation (ANSI).
+#define	SIGUSR2		12	/* User-defined signal 2 (POSIX).
+#define	SIGPIPE		13	/* Broken pipe (POSIX).
+#define	SIGALRM		14	/* Alarm clock (POSIX).
+#define	SIGTERM		15	/* Termination (ANSI).
+#define	SIGSTKFLT	16	/* Stack fault.
+#define	SIGCLD		SIGCHLD	/* Same as SIGCHLD (System V).
+#define	SIGCHLD		17	/* Child status has changed (POSIX).
+#define	SIGCONT		18	/* Continue (POSIX).
+#define	SIGSTOP		19	/* Stop, unblockable (POSIX).
+#define	SIGTSTP		20	/* Keyboard stop (POSIX).
+#define	SIGTTIN		21	/* Background read from tty (POSIX).
+#define	SIGTTOU		22	/* Background write to tty (POSIX).
+#define	SIGURG		23	/* Urgent condition on socket (4.2 BSD).
+#define	SIGXCPU		24	/* CPU limit exceeded (4.2 BSD).
+#define	SIGXFSZ		25	/* File size limit exceeded (4.2 BSD).
+#define	SIGVTALRM	26	/* Virtual alarm clock (4.2 BSD).
+#define	SIGPROF		27	/* Profiling alarm clock (4.2 BSD).
+#define	SIGWINCH	28	/* Window size change (4.3 BSD, Sun).
+#define	SIGPOLL		SIGIO	/* Pollable event occurred (System V).
+#define	SIGIO		29	/* I/O now possible (4.2 BSD).
+#define	SIGPWR		30	/* Power failure restart (System V).
+#define SIGSYS		31	/* Bad system call.
+#define SIGUNUSED	31
+
+*/
 
 void leerConfiguracion();
 void mandarRutaInicial(char* ruta);
 void gestionarTransformacion(peticionDeTransformacion pedido[], int tam);
 void pedir_transformacion(peticionDeTransformacion pedido[], int a);
 void gestionarReduccionLocal(peticionDeReduccionLocal pedido[], int tam);
+void gestionarReduccionGlobal(peticionDeReduccionGlobal pedido[], int tam);
 void pedir_reduccion();
+void signal_capturer(int numeroSeñal);
+void dividirPorCero(); //para ejemplo de señales
 
 
 
@@ -45,6 +97,8 @@ struct metricas {
 int main(void) {
 
 	leerConfiguracion();
+	signal(SIGFPE,signal_capturer);
+
 	peticionDeReduccionLocal reduLo[5];
 	mandarRutaInicial("/home/texto1.txt");
 	int tam = 3;
@@ -59,10 +113,13 @@ int main(void) {
 	pedido[2].direccion= "putoElQueLee";
 	pedido[2].worker="laburante";
 
+
+
+
 	gestionarTransformacion(pedido, tam);
 	gestionarReduccionLocal(reduLo, 5);
 
-*/
+
 	return EXIT_SUCCESS;
 
 }
@@ -132,12 +189,26 @@ void gestionarReduccionLocal(peticionDeReduccionLocal pedido[], int tam){
 
 }
 
-void crearDAtosParaReduccionLocal (indicacionesParaReduccionLocal solicitud[], int tam){}
-
-
-void pedir_reduccion(){
-
+void gestionarReduccionGlobal(peticionDeReduccionGlobal pedido[], int tam){
 
 
 }
 
+void crearDAtosParaReduccionLocal (indicacionesParaReduccionLocal solicitud, int tam){}
+
+
+void pedir_reduccion(){
+
+}
+
+void signal_capturer(int numeroSeñal){
+
+	switch(numeroSeñal){
+	case 8: printf("division por 0");
+	break;
+	}
+}
+void dividirPorCero(){
+	int a = 4/0;
+	return;
+}
