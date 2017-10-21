69bc3eeb3dd9f8391d9dffe810d1a2df5e8bd1f1
ahufa <facundoahumada93@hotmail.com>
2017-10-21 15:19:36 -0300

    Creo archivos para manejar las funciones de las tablas

---
+++ fileSystem/src/tablas.h
@@ -0,0 +1,67 @@
+#ifndef TABLAS_H_
+#define TABLAS_H_
+
+#include <commons/config.h>
+#include <commons/collections/list.h>
+#include <commons/string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <biblioteca/sockets.h>
+
+/*---------	---------------Estructuras-------------------------*/
+//Tabla de sockets
+typedef struct {
+	int socket;
+	char * nombre;
+} t_tabla_sockets;
+
+//Tabla de nodos
+typedef struct {
+	int tamanio;
+	int libres;
+	t_list * nomNodos; //Lista de nombres de los nodos
+	t_list * infoDeNodo; //Lista con cada nodo en particular
+} t_tabla_nodo;
+
+//Registro tabla de Directorios
+typedef struct {
+	int index;
+	char nombre[255];
+	int padre;
+} t_directory;
+
+
+/*------------------------Variables globales-------------------------*/
+t_list * tablaSockets;
+t_tabla_nodo * tablaNodos;
+t_list * tablaDirectorios;
+
+t_config * configTablaDirectorios;
+t_config * configTablaNodo;
+t_config * configTablaBitmap;
+
+/*-------------------------Tabla de directorios-------------------------*/
+void 				crearTablaDirectorios			(char *);
+void 				crearArchivoTablaDirectorios	(char *);
+void 				agregarDirectorioTabla			(t_directory *);
+void 				eliminarDirectorioTabla			(char *, int);
+void 				persistirTablaDirectorios		(void);
+
+/*-------------------------Tabla de nodos-------------------------*/
+void	 			crearTablaNodos					(char *);
+void 				crearArchivoTablaNodos			(char *);
+void 				agregarNodoTablaNodos			(t_nodo_info *);
+void 				eliminarNodoTablaNodos			(char *);
+void 				persistirTablaNodos				(void);
+
+/*-------------------------Tabla de sockets-------------------------*/
+void 				crearTablaSockets				(void);
+void 				agregarNodoTablaSockets			(char *, int);
+char * 				eliminarNodoTablaSockets		(int);
+
+/*-------------------------Tabla de Bitmap-------------------------*/
+void 				crearArchivoTablaBitmap			(t_nodo_info *);
+
+#endif /* TABLAS_H_ */
