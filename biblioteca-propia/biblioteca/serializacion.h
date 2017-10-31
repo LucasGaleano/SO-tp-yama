6675ab33d4de1cca65a49e2562a1b759bcfb3711
Lucas <l.a.galeano2@gmail.com>
2017-10-31 17:13:12 -0300

    otras cosas de conexiones

--- biblioteca-propia/biblioteca/serializacion.h
+++ biblioteca-propia/biblioteca/serializacion.h
@@ -12,21 +12,25 @@ typedef struct {
 } t_nodo_info;
 
 /*-------------------------Serializacion-------------------------*/
-void 			serializarHandshake				(t_paquete *, int);
-void 			serializarMensaje				(t_paquete *, char *);
-void 			serializarArchvivo				(t_paquete *, char *);
-void 			serializarInfoDataNode			(t_paquete *, char *, int, int);
-void 			serializarBloque				(t_paquete* unPaquete, char* bloque);
-void 			serializarSolicitudBloque		(t_paquete*, int);
+void 				serializarHandshake						(t_paquete *, int);
+void 				serializarMensaje						(t_paquete *, char *);
+void 				serializarArchvivo						(t_paquete *, char *);
+void 				serializarInfoDataNode					(t_paquete *, char *, int, int);
+void 				serializarBloque						(t_paquete* unPaquete, char* bloque);
+void 				serializarSolicitudLecturaBloque		(t_paquete*, int);
+void 				serializarSolicitudEscrituraBloque		(t_paquete*, void*, int);
+
 
 /*-------------------------Deserializacion-------------------------*/
-char * 			deserializarMensaje				(t_stream *);
-void * 			deserializarArchivo				(t_stream *);
-t_nodo_info *	deserializarInfoDataNode		(t_stream *);
-void*           deserializarBloque				(t_stream* buffer);
-int 			deserializarSolicitudBloque		(t_stream* buffer);
+char * 				deserializarMensaje						(t_stream *);
+void * 				deserializarArchivo						(t_stream *);
+t_nodo_info *		deserializarInfoDataNode				(t_stream *);
+void*           	deserializarBloque						(t_stream* buffer);
+int 				deserializarSolicitudLecturaBloque		(t_stream* buffer);
+t_pedidoEscritura*	deserializarSolicitudEscrituraBloque	(t_stream* buffer);
+
 
 /*-------------------------Funciones auxiliares-------------------------*/
-void * 			abrirArchivo					(char *, size_t *, FILE **);
+void * 			abrirArchivo							(char *, size_t *, FILE **);
 
 #endif /* BIBLIOTECA_SERIALIZACION_H_ */
