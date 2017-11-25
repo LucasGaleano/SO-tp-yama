b3867c457ab0c5e7120c3782536d1c7976fcaec0
ahufa <facundoahumada93@hotmail.com>
2017-11-25 12:25:16 -0300

    Se lee el archivo pidiendo los bloque al los nodos menos cargados historicamente

--- fileSystem/src/interfaz.h
+++ fileSystem/src/interfaz.h
@@ -1,10 +1,9 @@
 #ifndef INTERFAZ_H_
 #define INTERFAZ_H_
 
-#include <stdlib.h>
-#include <stdio.h>
-
 #include "tablas.h"
+
+t_list * listaTemporal;
 
 /*-------------------------Almacenar archivo-------------------------*/
 void				almacenarArchivo				(char *, char *, char *, int);
@@ -12,5 +11,10 @@ void * 				dividirBloqueArchivoBinario		(void *, int *);
 void * 				dividirBloqueArchivoTexto		(void *, int *);
 char * 				buscarNodoMenosCargado			(void);
 int 				buscarBloqueAEscribir			(char *);
+
+/*-------------------------Leer archivo-------------------------*/
+char* 				leerArchivo						(char *);
+t_tarea * 			nodoMenosSaturado				(char **, char **);
+int 				cantidadTareas					(char **);
 
 #endif /* INTERFAZ_H_ */
