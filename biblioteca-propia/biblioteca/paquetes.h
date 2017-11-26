2ecfbebe3b510e0d37de0b642ff321dd7e66802e
ahufa <facundoahumada93@hotmail.com>
2017-11-26 09:29:23 -0300

    Arreglo problemas de envio de paquetes

--- biblioteca-propia/biblioteca/paquetes.h
+++ biblioteca-propia/biblioteca/paquetes.h
@@ -14,41 +14,63 @@ void 							destruirPaquete						(t_paquete *);
 void 							mostrarPaquete						(t_paquete *);
 
 /*-----------------------------------Enviar paquetes-----------------------------------*/
-void 							enviarHandshake							(int, int);
-void 							enviarMensaje							(int, char *);
-void 							enviarArchivo							(int, char *);
-void 							enviarInfoDataNode						(int, char *, int, int);
-void 							enviarBloque							(int, char*);
-void 							enviarBloqueArchTemp					(int, char*, int);
-void 							enviarSolicitudLecturaBloque			(int, int);
-void 							enviarSolicitudLecturaArchTemp			(int, int, int);
-void 							enviarSolicitudEscrituraBloque			(int, void*,int);
-void 							enviarRespuestaEscrituraBloque			(int, bool, int);
-void 							enviarSolicitudTransformacion			(int, t_pedidoTransformacion *);
-void 							enviarSolicitudReduccionLocal			(int, t_pedidoReduccionLocal *);
-void 							enviarSolicitudReduccionGlobal			(int, t_pedidoReduccionGlobal *);
-void 							enviarSolicitudAlmacenadoFinal			(int, t_pedidoAlmacenadoFinal *);
-void 							enviarIndicacionTransformacion			(int, t_indicacionTransformacion *);
-void 							enviarIndicacionReduccionLocal			(int, t_indicacionReduccionLocal *);
-void 							enviarIndicacionReduccionGlobal			(int, t_indicacionReduccionGlobal *);
-void 							enviarIndicacionAlmacenadoFinal			(int, t_indicacionAlmacenadoFinal *);
+void 							enviarHandshake								(int, int);
+
+void 							enviarMensaje								(int, char *);
+
+void 							enviarArchivo								(int, char *);
+
+void 							enviarInfoDataNode							(int, char *, int, int);
+
+void 							enviarBloque								(int, char*);
+void 							enviarBloqueGenerarCopia					(int, char*, char*, char*);
+void 							enviarBloqueArchTemp						(int, char*, int);
+
+void 							enviarSolicitudLecturaBloque				(int, int);
+void 							enviarSolicitudLecturaBloqueGenerarCopia	(int, int, char*, char*);
+void 							enviarSolicitudLecturaArchTemp				(int, int, int);
+
+void 							enviarSolicitudEscrituraBloque				(int, void*,int);
+void 							enviarRespuestaEscrituraBloque				(int, bool, int);
+
+void 							enviarSolicitudTransformacion				(int, t_pedidoTransformacion *);
+void 							enviarSolicitudReduccionLocal				(int, t_pedidoReduccionLocal *);
+void 							enviarSolicitudReduccionGlobal				(int, t_pedidoReduccionGlobal *);
+void 							enviarSolicitudAlmacenadoFinal				(int, t_pedidoAlmacenadoFinal *);
+
+void 							enviarIndicacionTransformacion				(int, t_indicacionTransformacion *);
+void 							enviarIndicacionReduccionLocal				(int, t_indicacionReduccionLocal *);
+void 							enviarIndicacionReduccionGlobal				(int, t_indicacionReduccionGlobal *);
+void 							enviarIndicacionAlmacenadoFinal				(int, t_indicacionAlmacenadoFinal *);
 
 /*-----------------------------------Recibir paquetes-----------------------------------*/
-void 							recibirMensaje							(t_paquete *);
-void * 							recibirArchivo							(t_paquete *);
-void * 							recibirBloque							(t_paquete *);
-t_respuestaLecturaArchTemp * 	recibirBloqueArchTemp					(t_paquete *);
-int 							recibirSolicitudLecturaBloque			(t_paquete *);
-t_lecturaArchTemp * 			recibirSolicitudLecturaBloqueArchTemp	(t_paquete*);
-t_pedidoEscritura* 				recibirSolicitudEscrituraBloque			(t_paquete *);
-t_respuestaEscritura*			recibirRespuestaEscrituraBloque			(t_paquete *);
-t_pedidoTransformacion* 		recibirSolicitudTransformacion			(t_paquete *);
-t_pedidoReduccionLocal* 		recibirSolicitudReduccionLocal			(t_paquete *);
-t_pedidoReduccionGlobal*		recibirSolicitudReduccionGlobal			(t_paquete *);
-t_pedidoAlmacenadoFinal* 		recibirSolicitudAlmacenadoFinal			(t_paquete *);
-t_indicacionTransformacion* 	recibirIndicacionTransformacion			(t_paquete *);
-t_indicacionReduccionLocal*	 	recibirIndicacionReduccionLocal			(t_paquete *);
-t_indicacionReduccionGlobal*	recibirIndicacionReduccionGlobal		(t_paquete *);
-t_indicacionAlmacenadoFinal* 	recibirIndicacionAlmacenadoFinal		(t_paquete *);
+int 							recibirHandshake							(t_paquete *);
+
+char * 							recibirMensaje								(t_paquete *);
+
+void * 							recibirArchivo								(t_paquete *);
+
+t_nodo_info *					recibirInfoDataNode							(t_paquete *);
+
+void * 							recibirBloque								(t_paquete *);
+t_respuestaLecturaGenerarCopia* recibirBloqueGenerarCopia					(t_paquete *);
+t_respuestaLecturaArchTemp * 	recibirBloqueArchTemp						(t_paquete *);
+
+int 							recibirSolicitudLecturaBloque				(t_paquete *);
+t_lecturaGenerarCopia *			recibirSolicitudLecturaBloqueGenerarCopia	(t_paquete *);
+t_lecturaArchTemp * 			recibirSolicitudLecturaBloqueArchTemp		(t_paquete *);
+
+t_pedidoEscritura* 				recibirSolicitudEscrituraBloque				(t_paquete *);
+t_respuestaEscritura*			recibirRespuestaEscrituraBloque				(t_paquete *);
+
+t_pedidoTransformacion* 		recibirSolicitudTransformacion				(t_paquete *);
+t_pedidoReduccionLocal* 		recibirSolicitudReduccionLocal				(t_paquete *);
+t_pedidoReduccionGlobal*		recibirSolicitudReduccionGlobal				(t_paquete *);
+t_pedidoAlmacenadoFinal* 		recibirSolicitudAlmacenadoFinal				(t_paquete *);
+
+t_indicacionTransformacion* 	recibirIndicacionTransformacion				(t_paquete *);
+t_indicacionReduccionLocal*	 	recibirIndicacionReduccionLocal				(t_paquete *);
+t_indicacionReduccionGlobal*	recibirIndicacionReduccionGlobal			(t_paquete *);
+t_indicacionAlmacenadoFinal* 	recibirIndicacionAlmacenadoFinal			(t_paquete *);
 
 #endif /* SRC_PROCESAMIENTOPAQUETES_H_ */
